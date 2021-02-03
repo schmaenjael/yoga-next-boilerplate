9f32f2bb7b618d0bdd654730948f720a77354a72
heitzlki <kirill.heitzler@gmail.com>
2021-02-03 19:57:21 +0100

    TS types

---
+++ scr/types/graphql-utils.ts
@@ -0,0 +1,25 @@
+import { Redis } from 'ioredis';
+
+export interface Session extends Express.Session {
+  userId?: string;
+}
+
+export interface Context {
+  redis: Redis;
+  url: string;
+  session: Session;
+  req: Express.Request;
+}
+
+export type Resolver = (
+  parent: any,
+  args: any,
+  context: Context,
+  info: any
+) => any;
+
+export interface ResolverMap {
+  [key: string]: {
+    [key: string]: Resolver;
+  };
+}
