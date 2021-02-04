14f83639f6c734a3e96a956aaba44c540cb0edee
heitzlki <kirill.heitzler@gmail.com>
2021-02-04 21:16:58 +0100

    🚧feat: basic resolver structure

---
+++ scr/models/user/register/resolvers.ts
@@ -0,0 +1,21 @@
+// Envoierment variables
+
+// Imports
+import * as bcrypt from 'bcrypt';
+import * as yup from 'yup';
+
+// Utils
+import { ResolverMap } from '../../../types/graphql-utils';
+import { formatYupError } from '../../../utils/formatYuperror';
+
+// Alerts
+
+// Yup schema
+
+// Register resolver
+
+export const resolvers: ResolverMap = {
+  Mutation: {
+    register: async () => {},
+  },
+};
