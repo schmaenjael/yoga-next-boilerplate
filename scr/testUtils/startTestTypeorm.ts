7307ba16aafa9370b5eca4b2894c1f86d348e881
heitzlki <kirill.heitzler@gmail.com>
2021-02-14 21:46:54 +0100

    ✨feat: TypeORM test connection for jest

--- scr/testUtils/startTestTypeorm.ts
+++ scr/testUtils/startTestTypeorm.ts
@@ -1,10 +1,13 @@
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '..', '.env') });
+
 import { getConnectionOptions, createConnection } from 'typeorm';
 
 export const startTestTypeorm = async (resetDB: boolean = false) => {
   const connectionOptions = await getConnectionOptions(process.env.NODE_ENV);
   return createConnection({
     ...connectionOptions,
-    name: 'default',
     synchronize: resetDB,
     dropSchema: resetDB,
   });
