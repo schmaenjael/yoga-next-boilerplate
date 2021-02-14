d7db965690a0386dc00f66186aa607f7d05ebbe2
heitzlki <kirill.heitzler@gmail.com>
2021-02-14 16:08:29 +0100

    ✨feat: startTypeorm with ormconfig.ts

--- scr/database/startTypeorm.ts
+++ scr/database/startTypeorm.ts
@@ -2,40 +2,10 @@ import * as dotenv from 'dotenv';
 import * as path from 'path';
 dotenv.config({ path: path.resolve(__dirname, '..', '.env') });
 
-import { createConnection, ConnectionOptions, Connection } from 'typeorm';
-import { User } from './entity/User';
+import { getConnectionOptions, createConnection } from 'typeorm';
 
 export const startTypeorm = async () => {
-  const testConfig: ConnectionOptions = {
-    name: 'development',
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'test',
-    password: 'test',
-    database: 'test',
-    synchronize: true,
-    logging: false,
-    entities: [User],
-    migrations: ['migration/**/*.ts'],
-    subscribers: ['subscriber/**/*.ts'],
-    cli: {
-      entitiesDir: 'entity',
-      migrationsDir: 'migration',
-      subscribersDir: 'subscriber',
-    },
-  };
-  let connection: Connection;
-
-  connection = await createConnection(testConfig);
-  console.log(connection);
-  let user = new User();
-  user.email = 'a';
-  user.userName = 'a';
-  user.password = 'a';
-  const userRepo = connection.getRepository(User);
-
-  user = await userRepo.save(user);
-  await connection.synchronize(true);
+  const connectionOptions = await getConnectionOptions(process.env.NODE_ENV);
+  return createConnection(connectionOptions);
 };
 startTypeorm();
