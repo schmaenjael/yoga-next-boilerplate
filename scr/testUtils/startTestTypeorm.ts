6080519101e76a48c1e935d8d76af7b43cc487b6
heitzlki <heitzlki@pop-os.localdomain>
2021-02-15 18:32:03 +0100

    ✨feat: log message test typeorm connects or could not connect

--- scr/testUtils/startTestTypeorm.ts
+++ scr/testUtils/startTestTypeorm.ts
@@ -2,6 +2,8 @@ import * as dotenv from 'dotenv';
 import * as path from 'path';
 dotenv.config({ path: path.resolve(__dirname, '..', '.env') });
 
+import * as chalk from 'chalk';
+
 import { getConnectionOptions, createConnection } from 'typeorm';
 
 export const startTestTypeorm = async (resetDB: boolean = false) => {
@@ -10,5 +12,17 @@ export const startTestTypeorm = async (resetDB: boolean = false) => {
     ...connectionOptions,
     synchronize: resetDB,
     dropSchema: resetDB,
-  });
+  })
+    .then(() => {
+      console.log(
+        chalk.green(
+          `[*] Started TypeORM connection to ${chalk.underline(
+            process.env.TYPEORM_DATABASE_TEST,
+          )} in ${process.env.NODE_ENV} mode `,
+        ),
+      );
+    })
+    .catch((err) => {
+      console.log(chalk.red(`[/] Unable to start TypeORM \n ${err}`));
+    });
 };
