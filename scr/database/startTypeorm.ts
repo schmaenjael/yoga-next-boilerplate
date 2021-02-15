cfc8c677ef99d2aeef1b79d800e3e1ba692dc6a4
heitzlki <heitzlki@pop-os.localdomain>
2021-02-15 18:31:45 +0100

    ✨feat: log message typeorm connects or could not connect

--- scr/database/startTypeorm.ts
+++ scr/database/startTypeorm.ts
@@ -2,6 +2,8 @@ import * as dotenv from 'dotenv';
 import * as path from 'path';
 dotenv.config({ path: path.resolve(__dirname, '..', '.env') });
 
+import * as chalk from 'chalk';
+
 import { getConnectionOptions, createConnection } from 'typeorm';
 
 export const startTypeorm = async () => {
@@ -9,5 +11,17 @@ export const startTypeorm = async () => {
   return createConnection({
     ...connectionOptions,
     name: 'default',
-  });
+  })
+    .then(() => {
+      console.log(
+        chalk.green(
+          `[*] Started TypeORM connection to ${chalk.underline(
+            process.env.TYPEORM_DATABASE_DEV,
+          )} in ${process.env.NODE_ENV} mode `,
+        ),
+      );
+    })
+    .catch((err) => {
+      console.log(chalk.red(`[/] Unable to start TypeORM \n ${err}`));
+    });
 };
